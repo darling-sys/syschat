f86afd613027387dde2acdf6e89a2ebfceca77c2
Miss Darling <TiffanyErikaDarling@gmail.com>
2026-07-30 16:52:36 -0700

    Add files via upload

---
+++ source.cs
@@ -0,0 +1,184 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace syschat
+{
+	public class constance // helpful constance gives us info
+	{
+		public static readonly string version = "0.0.2";
+	}
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int sleepTime = 400;
+			if(args.Length > 0)
+			{
+				int.TryParse(args[0], out sleepTime);
+			}
+			data d = new data();
+			while(true)
+			{
+				printRoom(d);
+				Thread.Sleep(sleepTime);
+				if(Console.KeyAvailable)
+				{
+					ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+					if(pressedKey.KeyChar == '\\')
+					{
+						Console.Clear();
+						Console.Write("\\");
+						string input = Console.ReadLine();
+						input = input.Trim();
+						input = "\\" + input;
+						d = commandHandler(d, input);
+					}
+					else if(pressedKey.Key == ConsoleKey.Enter)
+					{
+						Console.Clear();
+						Console.Write(d.myName + " > ");
+						string input = Console.ReadLine();
+						input = input.Trim();
+						if(input.StartsWith("\\"))
+						{
+							d = commandHandler(d, input);
+						}
+						else
+						{
+							d = addMessage(d, d.myName, input);
+						}
+					}
+				}
+			}
+		}
+		public static data commandHandler(data d, string input)
+		{
+			switch(input)
+			{
+				case "\\help":
+				{
+					Console.Clear();
+					Console.WriteLine("Syschat Help: ");
+					Console.WriteLine("");
+					Console.WriteLine("press the enter key to start writing a message");
+					Console.WriteLine("press the enter key again to send it");
+					Console.WriteLine("using backslash before a message turns it into a command");
+					Console.WriteLine("");
+					Console.WriteLine("available commands:");
+					Console.WriteLine("front / switch / rp : all 3 of these change the name of the user");
+					Console.WriteLine("dump / log / dumplog : all 3 of these save the chat log to a file");
+					Console.WriteLine("ver / version : both of these print the syschat version number");
+					Console.WriteLine("quit : this closes syschat without saving the messages sent");
+					Console.WriteLine("");
+					Console.WriteLine("run syschat with a number as an argument to change the rendertime");
+					Console.WriteLine("");
+					Console.WriteLine("press any key to go back to chat rendering");
+					Console.WriteLine("");
+					Console.ReadKey();
+					return d;
+				}
+				case "\\quit":
+				{
+					Environment.Exit(0);
+					return d;
+				}
+				case "\\version":
+				{
+					d = addMessage(d, "system", constance.version);
+					return d;
+				}
+				case "\\ver":
+				{
+					d = addMessage(d, "system", constance.version);
+					return d;
+				}
+				case "\\front":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					return d;
+				}
+				case "\\switch":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					return d;
+				}
+				case "\\rp":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					return d;
+				}
+				case "\\dump":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
+				case "\\log":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
+				case "\\dumplog":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
+				default:
+				{
+					d = addMessage(d, "system", "unknown command");
+					return d;
+				}
+			}
+		}
+		public static void dumplog(data d, string path)
+		{
+			string full = "";
+			foreach(message m in d.messages)
+			{
+				full = full + m.fromName + " : " + m.content + "\n";
+			}
+			File.WriteAllText(path, full);
+		}
+		public static data addMessage(data d, string playerName, string messageContent)
+		{
+			message m = new message();
+			m.fromName = playerName;
+			m.content = messageContent;
+			d.messages.Add(m);
+			return d;
+		}
+		public static void printRoom(data d)
+		{
+			Console.Clear();
+			foreach(message m in d.messages)
+			{
+				Console.WriteLine(m.fromName + " : " + m.content);
+			}
+		}
+	}
+	public class data
+	{
+		public string myName = "me";
+		public List<message> messages = new List<message>();
+	}
+	public class message
+	{
+		public string fromName;
+		public string content;
+	}
+}
+
