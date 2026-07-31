3cdde3af8756c27814cba93c71669d1d7b3c71d7
Miss Darling <TiffanyErikaDarling@gmail.com>
2026-07-30 18:32:52 -0700

    Update source.cs

--- source.cs
+++ source.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 namespace syschat
 {
 	public class constance // helpful constance gives us info
@@ -29,12 +27,12 @@ namespace syschat
 					input = input.Trim();
 					d = commandHandler(d, input);
 				}
-				if(pressedKey.KeyChar == '/')
+				else if(pressedKey.KeyChar == '/')
 				{
 					Console.Clear();
 					Console.Write("/");
 					string input = Console.ReadLine();
-					input = "\\" + input;
+					input = "/" + input;
 					input = input.Trim();
 					d = commandHandler(d, input);
 				}
@@ -44,7 +42,7 @@ namespace syschat
 					Console.Write(d.myName + " > ");
 					string input = Console.ReadLine();
 					input = input.Trim();
-					if(input.StartsWith("\\"))
+					if(input.StartsWith("\\") || input.StartsWith("/"))
 					{
 						d = commandHandler(d, input);
 					}
@@ -53,12 +51,136 @@ namespace syschat
 						d = addMessage(d, d.myName, input);
 					}
 				}
+				else
+				{
+					// do nothing for now (this check will be useful later)
+				}
 			}
 		}
 		public static data commandHandler(data d, string input)
 		{
 			switch(input)
 			{
+				case "/help":
+				{
+					Console.Clear();
+					Console.WriteLine("Syschat Help: ");
+					Console.WriteLine("");
+					Console.WriteLine("press enter to start writing a message");
+					Console.WriteLine("press the enter key to send it");
+					Console.WriteLine("");
+					Console.WriteLine("slash or backslash  before a message turns it into a command");
+					Console.WriteLine("");
+					Console.WriteLine("available commands:");
+					Console.WriteLine("front / switch / rp / name / swap: all 5 of these change the name");
+					Console.WriteLine("dump / log / dumplog : all 3 of these save the chat log to a file");
+					Console.WriteLine("ver / version : both of these print the syschat version number");
+					Console.WriteLine("clear : this clears out the chat history without closing");
+					Console.WriteLine("quit / end : both of these close syschat without saving");
+					Console.WriteLine("nvm / no / n : all 3 of these do nothing");
+					Console.WriteLine("");
+					Console.WriteLine("press any key to go back to chat now");
+					Console.WriteLine("");
+					Console.ReadKey();
+					return d;
+				}
+				case "/clear":
+				{
+					d = clearMessages(d);
+					return d;
+				}
+				case "/nvm":
+				{
+					return d;
+				}
+				case "/no":
+				{
+					return d;
+				}
+				case "/n":
+				{
+					return d;
+				}
+				case "/end":
+				{
+					Environment.Exit(0);
+					return d;
+				}
+				case "/quit":
+				{
+					Environment.Exit(0);
+					return d;
+				}
+				case "/version":
+				{
+					d = addMessage(d, "system", constance.version);
+					return d;
+				}
+				case "/ver":
+				{
+					d = addMessage(d, "system", constance.version);
+					return d;
+				}
+				case "/front":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					d = addMessage(d, "system", newName + " swapped in");
+					return d;
+				}
+				case "/switch":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					d = addMessage(d, "system", newName + " swapped in");
+					return d;
+				}
+				case "/swap":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					d = addMessage(d, "system", newName + " swapped in");
+					return d;
+				}
+				case "/name":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					d = addMessage(d, "system", newName + " swapped in");
+					return d;
+				}
+				case "/rp":
+				{
+					Console.Clear();
+					Console.Write("name: ");
+					string newName = Console.ReadLine();
+					d.myName = newName.Trim();
+					d = addMessage(d, "system", newName + " swapped in");
+					return d;
+				}
+				case "/dump":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
+				case "/log":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
+				case "/dumplog":
+				{
+					dumplog(d, "log.txt");
+					return d;
+				}
 				case "\\help":
 				{
 					Console.Clear();
@@ -67,7 +189,7 @@ namespace syschat
 					Console.WriteLine("press enter to start writing a message");
 					Console.WriteLine("press the enter key to send it");
 					Console.WriteLine("");
-					Console.WriteLine("using backslash before a message turns it into a command");
+					Console.WriteLine("slash or backslash  before a message turns it into a command");
 					Console.WriteLine("");
 					Console.WriteLine("available commands:");
 					Console.WriteLine("front / switch / rp / name / swap: all 5 of these change the name");
